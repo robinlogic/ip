de9757b8e9efa02f5ef3a246beb0246af07651af
robinlogic <robstudios638@gmail.com>
2025-08-19 18:31:46 +0800

    lvl 0

---
+++ src/main/java/Dyke.java
@@ -0,0 +1,9 @@
+public class Dyke {
+    public static void main(String[] args) {
+        String top_Bar = "____________________________________________________________\n";
+        String middle_Bar = "____________________________________________________________\n";
+        String bottom_Bar = "____________________________________________________________\n";
+        System.out.println( top_Bar + " Hello! I'm DYKE\n What can I do for you?\n\n"
+                + middle_Bar + " Bye. Hope to see you again soon!\n" + bottom_Bar);
+    }
+}
